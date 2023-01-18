ed497cbf6171c12bda81c97277bcbefaf4c75453
Felizj17 <felizj17@gmail.com>
2023-01-17 19:11:10 -0500

    basic game added to home page

---
+++ familyFued.js
@@ -0,0 +1,191 @@
+let cardData
+const getCards = async () => {
+  url = `https://rickandmortyapi.com/api/character/[1,2,3,4,5]`
+  const res = await fetch(url)
+  const data = await res.json()
+  console.log(data)
+  cardData = data
+  createGameCards(data)
+}
+getCards()
+
+function createGameCards(data) {
+  data.forEach(el => {
+    const card = document.createElement('article')
+    card.setAttribute('class', 'game-card')
+    const innerCard = document.createElement('div')
+    innerCard.setAttribute('class', 'game-card-inner')
+    const front = document.createElement('div')
+    front.setAttribute('class', 'game-card-front')
+    const back = document.createElement('div')
+    back.setAttribute('class', 'game-card-back')
+    const backImage = document.createElement('img')
+    const image = document.createElement('img')
+    image.setAttribute('class', 'game-card-image')
+    const name = document.createElement('p')
+    name.setAttribute('class', 'game-card-title')
+    name.innerText = `${el.name}`
+    const select = document.createElement('button')
+    select.setAttribute('value', el.name)
+    select.setAttribute('class', 'select-button')
+    select.innerText = 'PLAY ME'
+    image.setAttribute('src', el.image)
+
+    back.append(backImage)
+    front.append(name, image, select)
+    innerCard.append(front, back)
+    card.append(innerCard)
+    document.getElementById('player-hand').append(card)
+
+
+    card.addEventListener('click', e => {
+      console.log(e.target.value)
+      const trashCards = document.querySelectorAll('article')
+      console.log(trashCards)
+      trashCards.forEach(el => {
+        if (e.target.value.split(' ')[0] !== el.innerText.split(' ')[0]) {
+          el.classList.add('none')
+        } else {
+          el.classList.remove('game-card')
+          el.classList.add('selected')
+        }
+      })
+      const computerChoice = Math.floor(Math.random() * 5)
+      console.log(computerChoice)
+      let results = document.createElement('h2')
+      results.setAttribute('id', 'results')
+      document.getElementById('computer-choice').after(results)
+      switch (computerChoice) {
+        case 0:
+          createComputerCard(data[0])
+          if (
+            e.target.value === 'Morty Smith' ||
+            e.target.value === 'Jerry Smith'
+          ) {
+            results.innerText = 'You Lose!'
+            results.classList.add('lose')
+          } else if (e.target.value === 'Rick Sanchez') {
+            results.innerText = 'DRAW'
+            results.classList.add('draw')
+          } else {
+            results.innerText = 'You Win!\n +50'
+            results.classList.add('win')
+
+          }
+
+          break
+        case 1:
+          createComputerCard(data[1])
+          if (
+            e.target.value === 'Summer Smith' ||
+            e.target.value === 'Beth Smith'
+          ) {
+            results.innerText = 'You Lose!'
+            results.classList.add('lose')
+          } else if (e.target.value === 'Morty Smith') {
+            results.innerText = 'DRAW'
+            results.classList.add('draw')
+          } else {
+            results.innerText = 'You Win!\n +50'         
+               results.classList.add('win')
+
+          }
+          break
+        case 2:
+          createComputerCard(data[2])
+          if (
+            e.target.value === 'Rick Sanchez' ||
+            e.target.value === 'Beth Smith'
+          ) {
+            results.innerText = 'You Lose!'
+            results.classList.add('lose')
+          } else if(e.target.value === 'Summer Smith'){
+            results.innerText = 'DRAW'
+            results.classList.add('draw')
+          }else {
+            results.innerText = 'You Win!\n +50'
+            results.classList.add('win')
+
+          }
+          break
+        case 3:
+          createComputerCard(data[3])
+          if (
+            e.target.value === 'Rick Sanchez' ||
+            e.target.value === 'Jerry Smith'
+          ) {
+            results.innerText = 'You Lose!'
+            results.classList.add('lose')
+          } else if(e.target.value === 'Beth Smith'){
+            results.innerText = 'DRAW'
+            results.classList.add('draw')
+          }else {
+            results.innerText = 'You Win!\n +50'
+            results.classList.add('win')
+
+          }
+          break
+        case 4:
+          createComputerCard(data[4])
+          if (
+            e.target.value === 'Summer Smith' ||
+            e.target.value === 'Morty Smith'
+          ) {
+            results.innerText = 'You Lose!'
+            results.classList.add('lose')
+          } else if(e.target.value === 'Jerry Smith'){
+            results.innerText = 'You Both Lose!'
+            results.classList.add('draw')
+          }else {
+            results.innerText = 'You Win!\n +50'
+            results.classList.add('win')
+
+          }
+          break
+        default:
+          console.log(err)
+      }
+      setTimeout(()=>{
+        document.getElementById('game').remove()
+        const game = document.createElement('section')
+        const playerDiv = document.createElement('div')
+        const computerDiv = document.createElement('div')
+        playerDiv.setAttribute('id', 'player-hand')
+        computerDiv.setAttribute('id', 'computer-choice')
+        game.setAttribute('id', 'game')
+        game.append(computerDiv,playerDiv)
+        document.querySelector('main').append(game)
+
+        createGameCards(cardData)
+      },5000 )
+
+    })
+  })
+}
+function createComputerCard(data) {
+  const card = document.createElement('article')
+  card.setAttribute('class', 'game-card-pc')
+  const innerCard = document.createElement('div')
+  innerCard.setAttribute('class', 'game-card-inner')
+  const front = document.createElement('div')
+  front.setAttribute('class', 'game-card-front')
+  const back = document.createElement('div')
+  back.setAttribute('class', 'game-card-back')
+  const backImage = document.createElement('img')
+  const image = document.createElement('img')
+  image.setAttribute('class', 'game-card-image')
+  const name = document.createElement('p')
+  name.setAttribute('class', 'game-card-title')
+  name.innerText = `${data.name}`
+  const select = document.createElement('button')
+  select.setAttribute('value', data.name)
+  select.setAttribute('class', 'select-button')
+  select.innerText = 'PLAY ME'
+  image.setAttribute('src', data.image)
+
+  back.append(backImage)
+  front.append(name, image, select)
+  innerCard.append(front, back)
+  card.append(innerCard)
+  document.getElementById('computer-choice').append(card)
+}
---
+++ main.js
@@ -0,0 +1,83 @@
+const createCards = data => {
+  const url = data.info.next
+  if(document.getElementById('next-button')){
+    document.getElementById('next-button').remove()
+  }
+  
+  data.results.forEach(el => {
+    let type = ' '
+    const card = document.createElement('article')
+    card.setAttribute('class', 'card')
+    const innerCard = document.createElement('div')
+    innerCard.setAttribute('class', 'card-inner')
+    const front = document.createElement('div')
+    front.setAttribute('class', 'card-front')
+    const back = document.createElement('div')
+    back.setAttribute('class', 'card-back')
+    const backImage = document.createElement('img')
+    const image = document.createElement('img')
+    image.setAttribute('class', 'card-image')
+    const name = document.createElement('h3')
+    name.setAttribute('class', 'card-title')
+    const infoSection = document.createElement('section')
+    infoSection.setAttribute('class', 'card-info')
+    const info = document.createElement('p')
+    infoSection.append(info)
+    info.innerText = `Gender: ${el.gender}\nOrigin: ${el.origin.name}\nLocation: ${el.location.name}\nSpecies: ${el.species}\nStatus: ${el.status} `
+    el.type ? (type = `(${el.type})`) : ' '
+    name.innerText = `${el.name} ${type}`
+    image.setAttribute('src', el.image)
+    back.append(backImage)
+    front.append(name, image, infoSection)
+    innerCard.append(front, back)
+    card.append(innerCard)
+    document.getElementById('card-list').append(card)
+  })
+  const link = document.createElement('button')
+  link.setAttribute('id','next-button')
+  document.getElementById('grid-right').append(link)
+  link.addEventListener('click', ()=>getNextPage(url))
+}
+document.querySelector('form').addEventListener('submit', async e => {
+  e.preventDefault()
+  
+  url = `https://rickandmortyapi.com/api/character/?name=${e.target.query.value}&status=${e.target.status.value}&species=${e.target.querySpecies.value}&type=${e.target.queryType.value}&gender=${e.target.queryGender.value}`
+  const res = await fetch(url)
+  const data = await res.json()
+  clearCards()
+  e.target.query.value = ''
+  e.target.status.value = ''
+  e.target.querySpecies.value = ''
+  e.target.queryType.value = ''
+  e.target.queryGender.value = ''
+  console.log(data)
+  createCards(data)
+})
+
+async function getNextPage(url) {
+  const res = await fetch(url)
+  const data = await res.json()
+  clearCards()
+  createCards(data)
+}
+document.getElementById('advance').addEventListener('click', e => {
+  document.getElementById('advance-search').classList.remove('none')
+})
+
+document.getElementById('cancel').addEventListener('click', () => {
+  document.getElementById('advance-search').classList.add('none')
+})
+const clearCards = () => {
+  document.getElementById('card-list').remove()
+  const cardList = document.createElement('section')
+  cardList.setAttribute('id', 'card-list')
+  document.querySelector('#grid-right').append(cardList)
+}
+document.getElementById('search-icon-id').addEventListener('click', () => {
+  const form = document.getElementById('form')
+  form.style.display = 'block'
+})
+document.getElementById('search-icon-id2').addEventListener('click', () => {
+  const form = document.getElementById('form')
+  form.style.display = 'none'
+})
